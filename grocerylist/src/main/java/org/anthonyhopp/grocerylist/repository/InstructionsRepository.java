e9bb08e8dba525d6233ea2b3c1a0f9901d639cba
anthonyhopp <119610450+anthonyhopp@users.noreply.github.com>
2023-04-05 21:13:37 -0500

    Delete grocerylist directory

--- grocerylist/src/main/java/org/anthonyhopp/grocerylist/repository/InstructionsRepository.java
+++
@@ -1,9 +0,0 @@
-package org.anthonyhopp.grocerylist.repository;
-
-import org.anthonyhopp.grocerylist.entity.Instructions;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructionsRepository extends JpaRepository<Instructions, Integer> {
-
-    
-}
--- grocerylist/src/main/java/org/anthonyhopp/grocerylist/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.anthonyhopp.grocerylist.service;
-
-import org.anthonyhopp.grocerylist.model.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-   User findByEmail(String email);
-   User save(UserRegistrationDto registration);
-}
